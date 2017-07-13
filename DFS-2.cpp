8d4d7b52e291e73a6ebb48adc152c49432ffa5d9
Yuke Wang <505008605@qq.com>
2017-07-13 10:09:46 -0700

    Graph Converter & Reader
    Complete the Graph Converter and Graph Reader part
    Convert the graph from tuple format to the CSR format with option 0 0 
    Read the CSR format to the program into array

--- DFS-2.cpp
+++ DFS-2.cpp
@@ -1,12 +1,5 @@
 #include "stdafx.h"
-
-#include <iostream>
-#include <thread>
-#include <stdlib.h>
-#include <deque>
-#include <conio.h>
-#include <windows.h>
-#include <condition_variable>
+#include "graph.h"
 
 using namespace std;
 
@@ -21,14 +14,14 @@ mutex mtx;
 #define e_num 10
 #define thresh_hold 2
 #define thread_num 2
-
                  //0     1   2     3  4  5  6
-int Beg[v_num] = { 0,    2,  3,    5, 6, 7, 8 };
-int Adj[e_num] = { 2, 4, 5,  3, 1, 5, 6, 6, 1 };
-bool visited[v_num] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+//int Beg[v_num] = { 0,    2,  3,    5, 6, 7, 8 };
+//int Adj[e_num] = { 2, 4, 5,  3, 1, 5, 6, 6, 1 };
+//bool visited[v_num] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
 deque<int>worker_local_stack;
 
-void DFS_master(deque<int>&root_local_stack)
+void DFS_master(deque<int>&root_local_stack, long int edge_num)
 {
 	while (!start)
 		Sleep(1);
@@ -44,7 +37,7 @@ void DFS_master(deque<int>&root_local_stack)
 
 			cout << "root:  " << cur << endl;
 
-			for (int i = Beg[cur]; i < Beg[cur + 1]&& i < e_num; i++)
+			for (int i = Beg[cur]; i < Beg[cur + 1]&& i < edge_num; i++)
 				if (!visited[Adj[i]])
 					root_local_stack.push_back(Adj[i]);
 
@@ -70,7 +63,7 @@ void DFS_master(deque<int>&root_local_stack)
 	}
 }
 
-void DFS_worker()
+void DFS_worker(long int edge_num)
 {
 	while (!ready)
 		Sleep(1);
@@ -88,13 +81,13 @@ void DFS_worker()
 
 			cout << "worker:  " << cur << endl;
 
-			for (int i = Beg[cur]; i < Beg[cur + 1] && i < e_num; i++)
+			for (int i = Beg[cur]; i < Beg[cur + 1] && i < edge_num; i++)
 				if (!visited[Adj[i]])
 					worker_local_stack.push_back(Adj[i]);
 		}
 
 		//Equal-size partition of the large frontier
-		/*int size = worker_local_stack.size();
+		int size = worker_local_stack.size();
 
 		if (size > thresh_hold)
 		{
@@ -106,28 +99,45 @@ void DFS_worker()
 			worker_local_stack.pop_front();
 			temp--;
 		}
-	}*/
+	  }
 	}
 	return;
 }
 
-
-int main()
+int main(int argc, char **argv)
 {
 	thread t[thread_num];
+
 	deque<int>g;
 	g.push_back(0);
 
-	
-	t[1] = thread(&DFS_worker);    // activate the worker first
+	if (argc < 3)
+	{
+		cout << "wrong number of arguments" << endl;
+		cout << "./exe graph_begin_file graph_csr_file" << endl;
+		exit(-1);
+	}
+
+	graph<long int,long int,long int,long int> *gr = new graph<long int,long int,long int,long int>(argv[1], argv[2]);
+
+	long int *Beg = new long int[gr->vert_count];
+	long int *Adj = new long int[gr->edge_count];
+
+	memcpy(Beg, gr->beg_pos, sizeof(long int) * gr->vert_count);
+	memcpy(Adj, gr->csr, sizeof(long int) * gr->edge_count);
+
+    bool *visited = new bool[gr->vert_count];
+    memset(visited, 0, gr->vert_count*sizeof(bool));
+
+	t[1] = thread(&DFS_worker, gr->edge_count);    // activate the worker first
 
 	start = true;
 	syn_start.notify_all();
-	t[0] = thread(&DFS_master, g);  //start up master thread
+	t[0] = thread(&DFS_master, g, gr->edge_count);  //start up master thread
 	
 
 	for (int thr = 0; thr < thread_num; thr++)
-		t[thr].join();                   // synchronization of all threads
+		t[thr].join();          // synchronization of all threads
 
     return 0;
 }
